193bc1281cb10a76b70dcc197cbb973438370ca4
Lakshmi kalavathi <56877693+lakshmikalaambati@users.noreply.github.com>
2019-11-29 15:35:24 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,12 +1,21 @@
 var arraylist=[];
-i=1;
-function add(){    
-  arraylist.push(document.getElementById('addtolist').value);
-  console.log(arraylist);
 
-        var title   =document.getElementById('addtolist').value;
-        var node = document.createElement('div');        
-        node.innerHTML = '<input type="checkbox" id="check' + i + '" name="check' + i + '"><label for="check' +i + '">'+ title +'</label>';       
-        document.getElementById('container').appendChild(node);    
+
+function addtolist(){    
+
+    arraylist.push(document.getElementById('addtolist').value);
+    console.log(arraylist);
+
+    var title   =document.getElementById('addtolist').value;
+    var node = document.createElement('div');        
+    node.innerHTML = '<div class="w-50"><input type="checkbox" onclick="strikeoff('+arraylist.length+')" id="check' + arraylist.length + '" name="check' + arraylist.length + '"><label class="m-1 text-white h5" for="check' +arraylist.length + '">'+ title   +'</label><button class="btn-xs text-white bg-warning rounded ml-5 pull-right" type="button"><small>Remove</button></small></div>';
+    console.log(node.innerHTML);
+    document.getElementById('addtolistsection').appendChild(node);    
+}
+
+function strikeoff(itemindex)
+{
+    var text ="check"+itemindex;
     
+   console.log(document.getElementById(text).value);
 }
