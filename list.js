4ef633b17fec02bcb92759fdd748b52ed1e05146
Lakshmi kalavathi <56877693+lakshmikalaambati@users.noreply.github.com>
2019-12-01 14:11:58 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,21 +1,68 @@
-var arraylist=[];
-
+//var arraylist=[];
+var count=1;
 
 function addtolist(){    
 
-    arraylist.push(document.getElementById('addtolist').value);
-    console.log(arraylist);
+    //arraylist.push(document.getElementById('addtolist').value);
+    //console.log(arraylist);
 
     var title   =document.getElementById('addtolist').value;
-    var node = document.createElement('div');        
-    node.innerHTML = '<div class="w-50"><input type="checkbox" onclick="strikeoff('+arraylist.length+')" id="check' + arraylist.length + '" name="check' + arraylist.length + '"><label class="m-1 text-white h5" for="check' +arraylist.length + '">'+ title   +'</label><button class="btn-xs text-white bg-warning rounded ml-5 pull-right" type="button"><small>Remove</button></small></div>';
-    console.log(node.innerHTML);
-    document.getElementById('addtolistsection').appendChild(node);    
+    var parentcontainer=document.getElementById('addtolistsection');
+        
+            var div = document.createElement('div'); 
+            div.className="w-75";
+            div.id="parent"+count;
+      
+            var checkbox = document.createElement('input'); 
+              
+            checkbox.type="checkbox"; 
+            checkbox.id="checkbox"+count;
+            checkbox.onclick=function () {strikeoff(this) };
+            
+            var label = document.createElement('label'); 
+             
+            label.htmlFor = "checkbox"+count; 
+            label.className ="m-1 text-white h5"; 
+            label.id="label"+count;
+            label.appendChild(document.createTextNode(title)); 
+              
+             var button= document.createElement('button'); 
+             button.className="btn-xs text-white bg-warning rounded ml-5 pull-right";
+             button.type="button";
+             button.onclick=function () {removeitem(this,) };
+             button.appendChild(document.createTextNode("Remove")); 
+    
+            var divelement =parentcontainer.appendChild(div);
+            divelement.appendChild(checkbox); 
+            divelement.appendChild(label); 
+            divelement.appendChild(button);
+     //concole.log(parentcontainer.innerHTML);
+    count=count+1;
+    document.getElementById("addtolist").value = "";
 }
 
-function strikeoff(itemindex)
+function strikeoff(idname)
 {
-    var text ="check"+itemindex;
     
-   console.log(document.getElementById(text).value);
+    var text=idname.id;
+    
+    var text1=document.getElementById(text).nextSibling.innerHTML;
+        
+    if(idname.checked ==true){
+    document.getElementById(text).nextSibling.innerHTML = "<del>"+text1+"</del>";
+    }
+    else if(idname.checked ==false){
+        document.getElementById(text).nextSibling.innerHTML = text1.replace("<del>","");
+    }
+   
+   alert(text1);
+}
+
+function removeitem(removeindex)
+{   
+
+   var removeitem=removeindex.parentElement;
+    removeitem.remove();
+   
+
 }
